79f08739f8c8dcdf5c276e45b958310e4afc066d
Damien Toh <personal@damienone.com>
2022-04-22 00:33:36 -0700

    q1 done

--- hw07.scm
+++ hw07.scm
@@ -1,20 +1,40 @@
 (define (cadr lst) (car (cdr lst)))
 
 (define (make-kwlist1 keys values)
-  'YOUR-CODE-HERE)
+  (cons keys (cons values nil))
+)
 
-(define (get-keys-kwlist1 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist1 kwlist)
+  (car kwlist)
+)
 
 (define (get-values-kwlist1 kwlist)
-  'YOUR-CODE-HERE)
+  (cadr kwlist)
+)
 
 (define (make-kwlist2 keys values)
-  'YOUR-CODE-HERE)
+  (if (null? keys)
+    nil
+  (cons
+    (cons (car keys) (cons (car values) nil))
+    (make-kwlist2 (cdr keys) (cdr values))
+  )
+  )
+)
 
-(define (get-keys-kwlist2 kwlist) 'YOUR-CODE-HERE)
+(define (get-keys-kwlist2 kwlist)
+  (if (null? kwlist)
+    nil
+    (cons (car (car kwlist)) (get-keys-kwlist2 (cdr kwlist)))
+  )
+)
 
 (define (get-values-kwlist2 kwlist)
-  'YOUR-CODE-HERE)
+  (if (null? kwlist)
+    nil
+    (cons (cadr (car kwlist)) (get-values-kwlist2 (cdr kwlist)))
+  )
+)
 
 (define (add-to-kwlist kwlist key value)
   'YOUR-CODE-HERE)
